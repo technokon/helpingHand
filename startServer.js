b0e6d741277d52508647f5b1b618bef15530f152
technokon <iakoupov@gmail.com>
2018-02-15 12:25:46 -0500

    added comment on how algolia index should be used

--- startServer.js
+++ startServer.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+// authenticated client should update algolia index
+// client side shoudl only hold search keys, but back end should have a key that
+// is allowed to CRUD algolia indexes
+// private key can be stored here and used when logged-in client has
+// created/updated or deleted the posting
+// create an api here that will do the update,
+// this api should also check for session token or something that verifies client is authenticated
 
 app.use(express.static('www'))
 app.listen(process.env.PORT || 3000)
