f638c6be1a876a0eb56f7c39c74598b068b9c46f
technokon <iakoupov@gmail.com>
2019-02-06 13:28:17 -0500

    removed unnecessary import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import {Component, ViewChild} from '@angular/core';
-import {NavController, Platform} from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {Component} from '@angular/core';
+import {Platform} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import {HomePage} from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild('mainNav') nav: NavController;
   rootPage:any = HomePage;
 
   constructor(
